17630e5cde8cc51571c91b6a7fe0332331e8571e
ishananda-dotcom <isha.nanda@kickdrumtech.com>
2026-06-12 00:23:41 +0530

    fix: revert wrong uploadedfile.py change

--- django/core/files/uploadedfile.py
+++ django/core/files/uploadedfile.py
@@ -79,9 +79,8 @@ class TemporaryUploadedFile(UploadedFile):
     def __init__(self, name, content_type, size, charset, content_type_extra=None):
         _, ext = os.path.splitext(name)
         file = tempfile.NamedTemporaryFile(
-            suffix=".upload" + ext, dir=settings.FILE_UPLOAD_TEMP_DIR, delete=False
+            suffix=".upload" + ext, dir=settings.FILE_UPLOAD_TEMP_DIR
         )
-        os.chmod(file.name, 0o644)
         super().__init__(file, name, content_type, size, charset, content_type_extra)
 
     def temporary_file_path(self):
