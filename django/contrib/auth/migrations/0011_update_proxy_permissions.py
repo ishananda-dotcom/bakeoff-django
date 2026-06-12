3d63447b26d0f49964f30f8483c7c464b874770e
ishananda-dotcom <isha.nanda@kickdrumtech.com>
2026-06-12 15:49:02 +0530

    fix: fix continuation line indentation

--- django/contrib/auth/migrations/0011_update_proxy_permissions.py
+++ django/contrib/auth/migrations/0011_update_proxy_permissions.py
@@ -20,10 +20,12 @@ def update_proxy_model_permissions(apps, schema_editor, reverse=False):
         permissions_query = Q(codename__in=proxy_default_permissions_codenames)
         for codename, name in opts.permissions:
             permissions_query = permissions_query | Q(codename=codename, name=name)
-        concrete_content_type = ContentType.objects.get_for_model(Model,
-            for_concrete_model=True)
-        proxy_content_type = ContentType.objects.get_for_model(Model,
-            for_concrete_model=False)
+        concrete_content_type = ContentType.objects.get_for_model(
+            Model, for_concrete_model=True
+        )
+        proxy_content_type = ContentType.objects.get_for_model(
+            Model, for_concrete_model=False
+        )
         old_content_type = proxy_content_type if reverse else concrete_content_type
         new_content_type = concrete_content_type if reverse else proxy_content_type
         # Delete any existing permissions with the new content type that would
