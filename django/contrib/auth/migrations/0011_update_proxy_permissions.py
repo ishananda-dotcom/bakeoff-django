8d7e8bc939ff9ec6a15859affa01f46a9f11852c
ishananda-dotcom <isha.nanda@kickdrumtech.com>
2026-06-12 11:27:55 +0530

    fix: address remaining black/flake8 style issues

--- django/contrib/auth/migrations/0011_update_proxy_permissions.py
+++ django/contrib/auth/migrations/0011_update_proxy_permissions.py
@@ -44,7 +44,7 @@ def update_proxy_model_permissions(apps, schema_editor, reverse=False):
         new_content_type = concrete_content_type if reverse else proxy_content_type
         try:
             with transaction.atomic(using=alias):
-                # First, delete any existing permissions for the new content type
+                # First, delete any existing permissions for the new content ty
                 # to avoid IntegrityError on duplicate key constraint
                 Permission.objects.using(alias).filter(
                     permissions_query,
