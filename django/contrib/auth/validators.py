3434a27ac82ad667a07bbb652263b6623ca52a88
ishananda-dotcom <isha.nanda@kickdrumtech.com>
2026-06-12 17:45:00 +0530

    Add regression tests for UsernameValidator

--- django/contrib/auth/validators.py
+++ django/contrib/auth/validators.py
@@ -23,3 +23,32 @@ class UnicodeUsernameValidator(validators.RegexValidator):
         "numbers, and @/./+/-/_ characters."
     )
     flags = 0
+
+
+# Regression tests for UsernameValidator
+
+import unittest
+
+class TestUsernameValidator(unittest.TestCase):
+    def test_valid_username(self):
+        validator = ASCIIUsernameValidator()
+        try:
+            validator('valid_username')
+        except validators.ValidationError:
+            self.fail('ASCIIUsernameValidator raised ValidationError unexpectedly!')
+
+    def test_invalid_username_with_newline(self):
+        validator = ASCIIUsernameValidator()
+        with self.assertRaises(validators.ValidationError):
+            validator('invalid_username
+')
+
+    def test_unicode_username(self):
+        validator = UnicodeUsernameValidator()
+        try:
+            validator('valid_username_123')
+        except validators.ValidationError:
+            self.fail('UnicodeUsernameValidator raised ValidationError unexpectedly!')
+
+if __name__ == '__main__':
+    unittest.main()
